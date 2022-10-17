e93eb0de8ef11427ea4a92525cd6b73a8b5914f1
Arsema-Zeweldi <arsugeb@gmail.com>
2022-10-17 19:16:39 +0000

    3 fix

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -19,8 +19,8 @@ if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/todos"
     tasks = requests.get(url, verify=False).json()
     [user_data.get(task.get("userId")).append({"task": task.get("title"),
-                                       "completed": task.get("completed"),
-                                       "username": all_user.get(
+                                               "completed": task.get("completed"),
+                                               "username": all_user.get(
                                                task.get("userId"))})
      for task in tasks]
     with open("todo_all_employees.json", 'w') as jsf:
