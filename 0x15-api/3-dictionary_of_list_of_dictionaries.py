3c3b80bd0319979af4e2edb5a438d6d76f3ad6fe
Arsema-Zeweldi <arsugeb@gmail.com>
2022-10-17 19:12:51 +0000

    3

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -7,26 +7,21 @@ import requests
 from sys import argv
 
 
-def json_format(uid):
-    """no uid"""
+if __name__ == '__main__':
     url = "https://jsonplaceholder.typicode.com/users"
-    users = requests.get(url + 'users').json()
-    task = requests.get(url + 'todos').json()
-    all_users = {}
-    userData = {}
+    users = requests.get(url, verify=False).json()
+    all_user = {}
+    user_data = {}
     for user in users:
-        user_id = user.get("id")
-        userData[user_id] = []
-        all_users[user_id] = user.get("username")
+        uid = user.get("id")
+        user_data[uid] = []
+        all_user[uid] = user.get("username")
     url = "https://jsonplaceholder.typicode.com/todos"
     tasks = requests.get(url, verify=False).json()
-    [userData.get(task.get("userId")).append({"task": task.get("title"),
-                                              "completed": task.get("completed"),
-                                              "username": undoc.get(task.get("userId"))})
-    for task in tasks]
+    [user_data.get(task.get("userId")).append({"task": task.get("title"),
+                                       "completed": task.get("completed"),
+                                       "username": all_user.get(
+                                               task.get("userId"))})
+     for task in tasks]
     with open("todo_all_employees.json", 'w') as jsf:
-        json.dump(userData, jsf)
-
-
-if __name__ == "__main__":
-    json_format()
+        json.dump(user_data, jsf)
