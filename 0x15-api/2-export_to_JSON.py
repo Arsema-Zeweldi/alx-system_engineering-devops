934cd678108446a6472e5e5605f7825c59e9e255
Arsema-Zeweldi <arsugeb@gmail.com>
2022-10-17 16:59:51 +0000

    Task 2

---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+export data in the JSON format
+"""
+import json
+import requests
+from sys import argv
+
+
+def json_format(employeeid):
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(employeeid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employeeid)
+    tasks = requests.get(url, verify=False).json()
+    name = user.get('username')
+    task = [{"task": task.get("title"),
+          "username": name,
+          "completed": task.get("completed")} for task in tasks]
+    JSON = {}
+    JSON[employeeid] = task
+    with open("{}.json".format(employeeid), 'w') as filejs:
+        json.dump(JSON, filejs)
+
+
+if __name__ == "__main__":
+    json_format(argv[1])
