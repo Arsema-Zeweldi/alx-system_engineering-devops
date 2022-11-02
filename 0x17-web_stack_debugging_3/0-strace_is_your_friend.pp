5ed708037796b4cec06c92c0b1b360ad8ecc3b22
Arsema-Zeweldi <arsugeb@gmail.com>
2022-11-02 17:41:16 +0000

    Fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,8 @@
 exec { 'fix-wordpress':
+  environment => ['DIR=/var/www/html/wp-settings.php',
+                  'OLD=phpp',
+                  'NEW=php'],
   command     => 'sudo sed -i "s/$OLD/$NEW/" $DIR',
-  path        => '/bin',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1]
 }
