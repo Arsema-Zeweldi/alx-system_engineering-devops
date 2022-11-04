952909edc4cda3bc902919d4131af58b1383556b
Arsema-Zeweldi <arsugeb@gmail.com>
2022-11-04 09:59:20 +0000

    1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles of
+the first 10 hot posts listed for a given subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """ if not valid subreddit, it prints None """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {'User-Agent': 'arsi_gebre'}
+    parameters = {'limit': 10}
+    request = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
+
+
+    if request.status_code == 404:
+        print(None)
+        return None
+
+    try:
+        data = request.json().get("data")
+        children = data.get("children")
+        for child in children[:10]:
+            post = child.get("data")
+            print(post.get("title"))
+    except ValueError:
+        print(None)
+        return None
