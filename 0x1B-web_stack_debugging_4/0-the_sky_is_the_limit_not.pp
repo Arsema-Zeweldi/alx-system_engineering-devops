ec03c7f40c853ee9d45c5224fccb1240614329c6
Arsema-Zeweldi <arsugeb@gmail.com>
2022-11-10 17:54:10 +0000

    fix error

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,6 +1,6 @@
 # The sky isn't the limit
 exec { 'file limit':
-  onlyif  => 'test -e /etc/default/nginx',
-  command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-  provider => shell,
+    onlyif   => 'test -e /etc/default/nginx',
+    command  => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
+    provider => shell,
 }
